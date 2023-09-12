2c0db11733f27db9a1aa1c8a7730880c62cad91e
@전성훈 / SUNG HOON CHON <letaem77@gmail.com>
2023-09-12 16:40:09 +0900

    feat: update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <footer>
+          not affiliated with Naver Corp.
+        </footer>
+      </body>
     </html>
   );
 }
