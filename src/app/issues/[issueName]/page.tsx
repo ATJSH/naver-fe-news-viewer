2a3335c2a8015170bfda6756388d9dd45cc68cf2
ATJSH <letaem77@gmail.com>
2023-09-12 15:55:24 +0900

    feat: update style & page

--- src/app/issues/[issueName]/page.tsx
+++ src/app/issues/[issueName]/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
+import Head from "next/head";
 import { FC } from "react";
 import { getIssueByIssueName, getIssues } from "../api";
+import { Metadata } from "next";
 
 const IssuePage: FC<{
   params: {
@@ -8,9 +10,10 @@ const IssuePage: FC<{
   };
 }> = async ({ params: { issueName } }) => {
   return (
-    <div>
+    <main>
       <p>
-        data from <a href="https://github.com/naver/fe-news">naver/fe-news</a>
+        &copy; data from{" "}
+        <a href="https://github.com/naver/fe-news">naver/fe-news</a>
       </p>
       <header
         style={{
@@ -26,7 +29,7 @@ const IssuePage: FC<{
           __html: await getIssueByIssueName(issueName)
         }}
       />
-    </div>
+    </main>
   );
 };
 
@@ -37,4 +40,16 @@ export async function generateStaticParams() {
   return issues.map((issue) => ({
     issueName: issue
   }));
+}
+
+export async function generateMetadata({
+  params: { issueName }
+}: {
+  params: {
+    issueName: string;
+  };
+}): Promise<Metadata> {
+  return {
+    title: `${issueName} | unofficial fe-news viewer`
+  };
 }
--- src/app/issues/page.tsx
+++ src/app/issues/page.tsx
@@ -10,7 +10,8 @@ const IssuesPage: FC = async () => {
     <main>
       <h1>fe-news Issues</h1>
       <p>
-        data from <a href="https://github.com/naver/fe-news">naver/fe-news</a>
+        &copy; data from{" "}
+        <a href="https://github.com/naver/fe-news">naver/fe-news</a>
       </p>
       <ul>
         {issues.map((issue) => (
