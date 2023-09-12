0cf10d23bb24c1eaee036a3fc2c1e93485d5cac6
ATJSH <letaem77@gmail.com>
2023-09-12 18:01:07 +0900

    feat: update main page wording

--- src/app/issues/page.tsx
+++ src/app/issues/page.tsx
@@ -8,10 +8,10 @@ const IssuesPage: FC = async () => {
 
   return (
     <main>
-      <h1>📰 naver/fe-news 뉴스레터 뷰어</h1>
+      <h1>📰 naver/fe-news 뉴스레터 뷰어 (비공식)</h1>
       <p>
-        &apos;네이버의 프론트엔드 기술 소식 큐레이션 뉴스레터&apos;의 비공식
-        뷰어
+        &apos;네이버의 프론트엔드 기술 소식 큐레이션 뉴스레터&apos;를 가장
+        편하게 보는 방법
         <br />
         &copy;<a href="https://github.com/naver/fe-news">Naver Corp.</a>, 모든
         저작권 보유
